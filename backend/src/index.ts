4219b8040f1350343ff8e2be541732a60613fd9e
andy23512 <andy23512@gmail.com>
2019-12-14 22:52:53 +0800

    Set base href in environment variable

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 'use strict';
 import express from 'express';
 import compression from 'compression';
+import { readFileSync, writeFileSync } from 'fs';
 
 const _port = 4100;
 const _app_folder = 'dist';
+const baseHref = process.env.BASE_HREF || '/';
+const indexHtml = readFileSync('./dist/index.html', { encoding: 'utf-8' });
+const resultHtml = indexHtml.replace(
+  '</head>',
+  `<script>window.__baseHref = '${baseHref}'</script></head>`
+);
+writeFileSync('./dist/index-output.html', resultHtml);
 
 const app = express();
 app.use(compression());
@@ -11,9 +19,9 @@ app.use(compression());
 // ---- SERVE STATIC FILES ---- //
 app.get('*.*', express.static(_app_folder, { maxAge: '1y' }));
 
-// ---- SERVE APLICATION PATHS ---- //
+// ---- SERVE APPLICATION PATHS ---- //
 app.all('*', function(req, res) {
-  res.status(200).sendFile(`/`, { root: _app_folder });
+  res.status(200).sendFile(`/index-output.html`, { root: _app_folder });
 });
 
 // ---- START UP THE NODE SERVER  ----
